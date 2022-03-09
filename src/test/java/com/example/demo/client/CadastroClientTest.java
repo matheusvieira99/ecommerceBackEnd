a7dcb660c2062efe7ab877513ccd984e7d9a40b5
Matheus Vieira <matheus.vieira@ilabqa.com>
2022-03-09 13:16:46 -0300

    fix: test

--- src/test/java/com/example/demo/client/CadastroClientTest.java
+++ src/test/java/com/example/demo/client/CadastroClientTest.java
@@ -1,8 +1,14 @@
 package com.example.demo.client;
 
+
+
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 
-import org.junit.Test;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -19,11 +25,12 @@ public class CadastroClientTest {
 			MediaType mediaType = MediaType.parse("application/json");
 			RequestBody body = RequestBody.create(mediaType, "{\r\n    \"email\": \"fulano@gmail.com\",\r\n    \"senha\": \"123\"\r\n}");
 			Request request = new Request.Builder()
-			  .url("localhost:8082/cliente/cadastro")
+			  .url("http://localhost:8082/cliente/cadastro")
 			  .method("POST", body)
 			  .addHeader("Content-Type", "application/json")
 			  .build();
 			Response response = client.newCall(request).execute();
+			assertEquals(201, response.code());
 			
 
 	}
