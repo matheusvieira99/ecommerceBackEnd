6da8c92c5a81050b5dbae15cde9e31ea27cc621b
Matheus Vieira <matheus-vieira12@hotmail.com>
2022-03-11 10:10:51 -0300

    commit

--- src/test/java/com/example/demo/client/CadastroClientTest.java
+++ src/test/java/com/example/demo/client/CadastroClientTest.java
@@ -18,10 +18,10 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+@DisplayName("Test case - test")
 public class CadastroClientTest {
 	
 	@Test
-	@DisplayName("Test case - test")
 	public void testeUnitario() throws IOException {
 	OkHttpClient client = new OkHttpClient().newBuilder()
 			  .build();
